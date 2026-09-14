b5be82685be3941e6c41761e66b6968774de23b9
Abdallah Beraidais <abdallah.beraidais@gmail.com>
2026-09-14 11:35:25 +0100

    Solve 2 Challenges from Codeware

---
+++ greet.js
@@ -0,0 +1,9 @@
+/*
+-- Name : Returning Strings
+-- URL  : https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+-- Languages : JavaScript
+*/
+
+function greet(name){
+  return "Hello, "+ name +" how are you doing today?"
+}
