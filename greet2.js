56e1954c2d5526d06551cfb8c34756930db40f6c
Abdallah Beraidais <abdallah.beraidais@gmail.com>
2026-09-15 14:18:58 +0100

    Solve 1 Challenge

---
+++ greet2.js
@@ -0,0 +1,8 @@
+/*
+-- Name : Grasshopper - Personalized Message
+-- URL  : https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
+-- Languages : JavaScript
+*/
+function greet (name, owner) {
+  return name == owner ? 'Hello boss':'Hello guest'
+}
---
+++ makeNegative.js
@@ -0,0 +1,8 @@
+/*
+-- Name : Return Negative
+-- URL  : https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+-- Languages : JavaScript
+*/
+function makeNegative(num) {
+  return num < 0 ? num : -num
+}
